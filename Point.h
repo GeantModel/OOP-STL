68834ca8c91b37ebef9f3c918e738f723928d806
Yakov <jeykeyel19@gmail.com>
2017-12-15 22:41:31 +0300

    qq

---
+++ Point.h
@@ -0,0 +1,24 @@
+
+#ifndef OOP_RAB5_POINT_H
+#define OOP_RAB5_POINT_H
+class Point{
+private: double x, y, z;
+public:
+    Point(double x, double y, double z) : x(x), y(y), z(z) {}
+    double getX() const {
+        return x;
+    }
+    double getY() const {
+        return y;
+    }
+    double getZ() const {
+        return z;
+    }
+    double getRast(){
+        return sqrt(x*x+y*y+z*z);
+    }
+    static double getRast2(Point x1, Point x2){
+        return sqrt(((x1.getX()-x2.getX()))*(x1.getX()-x2.getX()) + (x1.getY()-x2.getY())*(x1.getY()-x2.getY()) + (x1.getZ()-x2.getZ())*(x1.getZ()-x2.getZ()));
+    }
+};
+#endif //OOP_RAB5_POINT_H
---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "vector"
+#include "random"
+#include "time.h"
+#include "Point.h"
+using namespace std;
+int main() {
+    srand(time(0));
+    vector<Point> coord;
+    for(int i=0; i<50; i++){
+        Point Ai(rand()%100-rand()%100,rand()%100-rand()%100,rand()%100-rand()%100);
+        coord.push_back(Ai);
+        cout<<"("<<Ai.getX()<<", "<<Ai.getY()<<", "<<Ai.getZ()<<")"<<endl;
+    }
+    double k=0;
+    for(int i=0; i<=49; i++){
+        if (coord[i].getRast()<coord[k].getRast())
+            k=i;
+    }
+    cout<<"Minimal'naya k centru tochka "<<"("<<coord[k].getX()<<", "<<coord[k].getY()<<", "<<coord[k].getZ()<<")"<<endl;
+    vector<Point> A;
+    Point A1(-100,-100,-100);
+    Point A2(-100,100,-100);
+    Point A3(100,100,-100);
+    Point A4(100,-100,-100);
+    Point A5(100,-100,100);
+    Point A6(-100,-100,100);
+    Point A7(-100,100,100);
+    Point A8(100,100,100);
+    A.push_back(A1);A.push_back(A2);A.push_back(A3);A.push_back(A4);
+    A.push_back(A5);A.push_back(A6);A.push_back(A7);A.push_back(A8);
+    for (int i=0; i<8; i++){
+        k=0;
+        for (int j=0; j<50; j++){
+            if (Point::getRast2(coord[j],A[i])<Point::getRast2(coord[k],A[i]))
+                k=i;
+        }
+        cout<<"Minimal'naya k "<<"("<<A[i].getX()<<", "<<A[i].getY()<<", "<<A[i].getZ()<<")  "<<"("<<coord[k].getX()<<", "<<coord[k].getY()<<", "<<coord[k].getZ()<<")"<<endl;
+    }
+}
