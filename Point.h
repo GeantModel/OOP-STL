46a2a308c29977d3f431773804685ae4a7d42aa1
Yakov <jeykeyel19@gmail.com>
2017-12-17 23:49:48 +0300

    qq

--- Point.h
+++ Point.h
@@ -20,5 +20,9 @@ public:
     static double getRast2(Point x1, Point x2){
         return sqrt(((x1.getX()-x2.getX()))*(x1.getX()-x2.getX()) + (x1.getY()-x2.getY())*(x1.getY()-x2.getY()) + (x1.getZ()-x2.getZ())*(x1.getZ()-x2.getZ()));
     }
+
+    bool operator<(const Point &P) const {
+        return (x*x + y*y + z*z) < ((P.x*P.x)+(P.y*P.y)+(P.z*P.z));
+    }
 };
 #endif //OOP_RAB5_POINT_H
--- main.cpp
+++ main.cpp
@@ -3,20 +3,25 @@
 #include "random"
 #include "time.h"
 #include "Point.h"
+#include "algorithm"
+#include "map"
+
 using namespace std;
 int main() {
     srand(time(0));
+    int n=50;
     vector<Point> coord;
-    for(int i=0; i<50; i++){
+    for(int i=0; i<n; i++){
         Point Ai(rand()%100-rand()%100,rand()%100-rand()%100,rand()%100-rand()%100);
         coord.push_back(Ai);
         cout<<"("<<Ai.getX()<<", "<<Ai.getY()<<", "<<Ai.getZ()<<")"<<endl;
     }
     double k=0;
-    for(int i=0; i<50; i++){
+    for(int i=0; i<n; i++){
         if (coord[i].getRast()<coord[k].getRast())
             k=i;
     }
+    cout << endl;
     cout<<"Minimal'naya k centru tochka "<<"("<<coord[k].getX()<<", "<<coord[k].getY()<<", "<<coord[k].getZ()<<")"<<endl;
     vector<Point> A;
     Point A1(-100,-100,-100);
@@ -29,12 +34,23 @@ int main() {
     Point A8(100,100,100);
     A.push_back(A1);A.push_back(A2);A.push_back(A3);A.push_back(A4);
     A.push_back(A5);A.push_back(A6);A.push_back(A7);A.push_back(A8);
+    cout << endl;
     for (int i=0; i<8; i++){
         k=0;
-        for (int j=0; j<50; j++){
+        for (int j=0; j<n; j++){
             if (Point::getRast2(coord[j],A[i])<Point::getRast2(coord[k],A[i]))
                 k=j;
         }
         cout<<"Minimal'naya k "<<"("<<A[i].getX()<<", "<<A[i].getY()<<", "<<A[i].getZ()<<")  "<<"("<<coord[k].getX()<<", "<<coord[k].getY()<<", "<<coord[k].getZ()<<")"<<endl;
     }
+    sort(coord.begin(),coord.end());
+    map<int, Point> M;
+    for (int i=0; i<n; i++) {
+        M.emplace((i+1),coord[i]);
+    }
+    cout << endl;
+    for (auto& x: M) {
+        cout << x.first <<" "<<"(" << x.second.getX()<<" "<<x.second.getY()<<" "<<x.second.getZ()<<")"<<endl;
+    }
+
 }
